333967d9b9a1f6adfe22e3d8e787cde9f1afcf6f
daddyDOT <ahmedspahiconline@gmail.com>
2024-09-16 11:09:40 +0200

    core: Added `cors` to backend
    - To allow communication with frontend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const http = require('http');
 
 const app = express();
-const server = require('http').createServer(app);
+app.use(cors());
+
+const server = http.createServer(app);
+
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
