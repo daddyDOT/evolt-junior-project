bd84f2d1211ee455e04fbb752589935a79e17c94
daddyDOT <ahmedspahiconline@gmail.com>
2024-09-16 11:12:13 +0200

    feat: Added socket.io integration to backend server

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('(backend) A user connected:', socket.id);
+
+  socket.on('message', (data) => {
+    console.log('(backend) Message received:', data);
+    io.emit('message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('(backend) User disconnected:', socket.id);
+  });
+});
 
 const port = process.env.PORT || 5000;
 
