48762994e159718367c9725252f5357dac5971ee
daddyDOT <ahmedspahiconline@gmail.com>
2024-09-18 21:15:24 +0200

    refactor: Updated tailwind.config.ts to include @nextui-org/theme in content

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -4,7 +4,8 @@ import type { Config } from 'tailwindcss'
 export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}", 
-    "./app/*.{js,ts,jsx,tsx}"
+    "./app/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
