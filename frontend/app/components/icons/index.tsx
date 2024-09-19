e2bbda223585f7b42275f127effef486afdaa3a9
daddyDOT <ahmedspahiconline@gmail.com>
2024-09-19 10:30:04 +0200

    feat: Added MessageIcon component

--- frontend/app/components/icons/index.tsx
+++ frontend/app/components/icons/index.tsx
@@ -1,5 +1,6 @@
 import SendIcon from "./SendIcon";
 import NotificationIcon from "./NotificationIcon";
 import TrashIcon from "./TrashIcon";
+import MessageIcon from "./MessageIcon";
 
-export { SendIcon, NotificationIcon, TrashIcon };
+export { SendIcon, NotificationIcon, TrashIcon, MessageIcon };
