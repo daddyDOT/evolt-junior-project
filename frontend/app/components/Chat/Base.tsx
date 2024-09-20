08ce61382c0ec060c78b3f10afcd4bde4740903b
daddyDOT <ahmedspahiconline@gmail.com>
2024-09-20 11:49:47 +0200

    refactor: Updated Base component to listen for changes in Header component as well
    - Since it will look for both values every time the screen changes - I used useMemo to avoid saving the final value if it didn't change

--- frontend/app/components/Chat/Base.tsx
+++ frontend/app/components/Chat/Base.tsx
@@ -1,6 +1,6 @@
 import { Divider } from "@nextui-org/react"
 
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode, useEffect, useMemo, useState } from 'react';
 
 interface BaseProps {
   heading: string;
@@ -12,19 +12,22 @@ const Base = ({ heading, description, children }: BaseProps) => {
   const [headerHeight, setHeaderHeight] = useState<number>(0);
   const [screenHeight, setScreenHeight] = useState<number>(0);
 
+  const height = useMemo(() => Number(screenHeight) - Number(headerHeight), [screenHeight, headerHeight]);
+
   useEffect(() => {
+    const elementWithIdHeader = document.getElementById('header');
+    
     const updateScreenHeight = () => {
+      if (elementWithIdHeader) {
+        setHeaderHeight(elementWithIdHeader.offsetHeight);
+      }
       setScreenHeight(window.innerHeight);
     };
 
-    const elementWithIdHeader = document.getElementById('header');
-    if (elementWithIdHeader) {
-      setHeaderHeight(elementWithIdHeader.offsetHeight);
-    }
     updateScreenHeight();
-
+  
     window.addEventListener('resize', updateScreenHeight);
-
+  
     // Cleanup event listener
     return () => {
       window.removeEventListener('resize', updateScreenHeight);
@@ -32,7 +35,7 @@ const Base = ({ heading, description, children }: BaseProps) => {
   }, []);
 
   return (
-    <div className={`bg-default-100 md:rounded-3xl w-full md:!h-full p-4 md:p-8 overflow-hidden flex flex-col justify-between gap-4 md:gap-8`} style={{ height: `${screenHeight - headerHeight}px` }}>
+    <div className={`bg-default-100 md:rounded-3xl w-full md:!h-full p-4 md:p-8 overflow-hidden flex flex-col justify-between gap-4 md:gap-8`} style={{ height: height }}>
       <div className="hidden md:block">
         <h1 className="text-default-900 text-md md:text-2xl font-bold md:mb-1">{heading}</h1>
         <span className="text-default-500 text-xs md:text-sm">{description}</span>
