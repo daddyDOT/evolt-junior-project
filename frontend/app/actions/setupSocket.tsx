2bd47b87f782ae432c0fe92b53e90e3d9729381e
daddyDOT <ahmedspahiconline@gmail.com>
2024-09-21 18:24:57 +0200

    refactor: Updated frontend API and socket URLs in configuration files
    - Also described frontend as dependent of backend

--- frontend/app/actions/setupSocket.tsx
+++ frontend/app/actions/setupSocket.tsx
@@ -2,7 +2,7 @@ import { io, Socket } from "socket.io-client";
 import { Message, User } from "../interfaces";
 import { toast } from "react-toastify";
 
-const apiUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_SOCKET_URL : undefined;
+const apiUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_SOCKET_URL : undefined;
 
 interface SocketSetupParams {
   setSocket: (socket: Socket) => void;
